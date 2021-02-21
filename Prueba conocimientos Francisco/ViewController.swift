ee07ffbdba16a8d8814892a3b7369c9846ccd7ef
pacog.escamilla@gmail.com <pacog.escamilla@gmail.com>
2021-02-21 12:15:10 -0600

    Initial Commit

---
+++ Prueba conocimientos Francisco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba conocimientos Francisco
+//
+//  Created by Francisco Guerrero Escamilla on 21/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
